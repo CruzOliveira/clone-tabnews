29632613b03b2ff20dd60c5d00ac1330a6c0c09f
Matheus Cruz <80063865+CruzOliveira@users.noreply.github.com>
2025-12-02 11:38:59 +0000

    Adicionando arquivo pages

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+    }}>
+      <img src="https://i.ytimg.com/vi/T98RCOMZNLc/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAjjQ1RmgX0k_4oMckmiHnvWw0HGw" alt="Descrição da imagem" 
+      style={{ width: '300px', height: '200px' }}
+      />
+      <h1>Para a galera que esta comentando ae...</h1>
+    </div>
+    );
+}
+
+export default Home;
